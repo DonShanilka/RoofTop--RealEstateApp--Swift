79c7ccf208bf94f800a765b498f99d8fcca5fd81
DonShanilka <shanilka799@gmail.com>
2025-04-23 17:29:01 +0530

    create serchbar and location in home screen

---
+++ RoofTop_RealEstateApp/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  RoofTop_RealEstateApp
+//
+//  Created by Nishith Shanilka on 2025-04-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }
+            
+            ExploreView()
+                .tabItem {
+                    Label("Explore", systemImage: "magnifyingglass")
+                }
+            
+            FavoriteView()
+                .tabItem {
+                    Label("Favorite", systemImage: "heart.fill")
+                }
+            
+            ProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: "person.fill")
+                }
+        }
+        .accentColor(.blue) // Customize selected tab color
+    }
+}
