66916fe95a1cfca7bfd9346f7d4cf9258ea34bdc
DonShanilka <shanilka799@gmail.com>
2025-04-23 13:51:04 +0530

    Initial Commit

---
+++ RoofTop_RealEstateApp/RoofTop_RealEstateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoofTop_RealEstateAppApp.swift
+//  RoofTop_RealEstateApp
+//
+//  Created by Nishith Shanilka on 2025-04-23.
+//
+
+import SwiftUI
+
+@main
+struct RoofTop_RealEstateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
